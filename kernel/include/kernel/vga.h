5693f6878ed4649a85e3f0353ffd6cf23598bfcf
Benjamin Streb <Bstreb@gmail.com>
2014-12-09 08:29:33 -0800

    Changed vga memory pointer into a named constant
    As opposed to a magic constant and following along the meatier skeleton.
    In my opinion, this is one of the more interesting aspects of OS (and
    driver) development. In application development, something like this -
    taking a non-zero integer literal and turning it into a pointer would
    surely be wrong. In OS development, though, this is correct (if not very
    resilient against memory corruption).

--- kernel/include/kernel/vga.h
+++ kernel/include/kernel/vga.h
@@ -34,4 +34,5 @@ uint16_t make_vgaentry(char c, uint8_t color)
  
 static const size_t VGA_WIDTH = 80;
 static const size_t VGA_HEIGHT = 25;
- 
+
+static uint16_t* const VGA_MEMORY = (uint16_t*) 0xB8000;
