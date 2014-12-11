6df7fec1b113ccde0beaa63390990a927dc79588
Benjamin Streb <Bstreb@gmail.com>
2014-12-11 08:46:47 -0800

    Added kernel_early for setup stuff
    In case I eventually want global constructors, I split this stuff out. I
    also trimmed the comments back a lot, since this isn't the place for it.

--- kernel/arch/i386/kernel.c
+++ kernel/arch/i386/kernel.c
@@ -9,10 +9,13 @@
 #if defined(__cplusplus)
 extern "C" /* Use C linkage for kernel_main. */
 #endif
-void kernel_main()
+
+void kernel_early()
 {
     terminal_initialize();
-    /* Since there is no support for newlines in terminal_putchar yet, \n will
-       produce some VGA specific character instead. This is normal. */
+}
+
+void kernel_main()
+{
     terminal_writestring("Hello, kernel World!\n");
 }
