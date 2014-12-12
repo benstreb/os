3071d92a8809dd6a88ba5529e61cfaa6954c1dc8
Benjamin Streb <Bstreb@gmail.com>
2014-12-11 16:53:54 -0800

    C++ is not currently supported
    Don't pretend that it is

--- kernel/arch/i386/kernel.c
+++ kernel/arch/i386/kernel.c
@@ -6,10 +6,6 @@
 #error "You need to set up and use the cross compiler to build this OS"
 #endif
  
-#if defined(__cplusplus)
-extern "C" /* Use C linkage for kernel_main. */
-#endif
-
 void kernel_early()
 {
     terminal_initialize();
