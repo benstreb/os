35c87b37fe8f7ea1f663b84bcb79e431b42642ce
Benjamin Streb <Bstreb@gmail.com>
2014-12-11 16:57:06 -0800

    Trimmed some comments I forgot earlier

--- kernel/arch/i386/boot.s
+++ kernel/arch/i386/boot.s
@@ -35,17 +35,11 @@ _start:
     # Main entry point
     call kernel_main
 
-    # In case the function returns, we'll want to put the computer into an
-    # infinite loop. To do that, we use the clear interrupt ('cli') instruction
-    # to disable interrupts, the halt instruction ('hlt') to stop the CPU until
-    # the next interrupt arrives, and jumping to the halt instruction if it ever
-    # continues execution, just to be safe. We will create a local label rather
-    # than real symbol and jump to there endlessly.
+    # Hang if kernel_main returns
     cli
     hlt
 .Lhang:
     jmp .Lhang
 
-# Set the size of the _start symbol to the current location '.' minus its start.
-# This is useful when debugging or when you implement call tracing.
+# Set the size of the _start symbol
 .size _start, . - _start
