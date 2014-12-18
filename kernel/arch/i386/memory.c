d1fc735ce865855774aabae9f6be8e44a094fadf
Benjamin Streb <Bstreb@gmail.com>
2014-12-18 08:44:35 -0800

    Preliminary code to detect memory regions
    It compiles, but I doubt it works yet

---
+++ kernel/arch/i386/memory.c
@@ -0,0 +1,24 @@
+#include <stdint.h>
+
+#pragma pack(1)
+struct memory_region
+{
+    int64_t address;
+    int64_t size;
+    int32_t type;
+    int32_t extended_attributes;
+};
+
+
+void read_region(struct memory_region *region)
+{
+    region->extended_attributes=1;
+    __asm__("movl %0, %%es" : : "r" (region));
+    __asm__("movl %0, %%eax;\
+             movl %1, %%ebx;\
+             movl %2, %%ecx;\
+             movl %3, %%edx" :
+            : "i" (0xE820), "i" (0), "i" (24), "i" (0x534D4150)
+            : "%eax", "%ebx", "%ecx", "%edx");
+    __asm__("int %0" : : "i" (0x15) : "memory");
+}
