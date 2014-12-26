3bd28022da881c99f3659690ead72f7fb4d56eaf
Benjamin Streb <Bstreb@gmail.com>
2014-12-25 22:47:39 -0800

    Added string.h. No definitions yet
    This is needed for some libgcc functionality. More pressingly, I'd also
    like to use memcmp for testing num_to_chars and this seems like a good
    time to add all of these functions.
    Incidentally, I find it interesting that the mem* functions are found in
    string.h. It would have struck me that since they don't operate on
    null-terminated strings (what I typically refer to when I mention
    strings in C), they ought to appear in a mem.h or something like that.
    Chalk it up as another design quirk of C, I guess. While I don't think
    that C has any horrible design decisions, given its age, I do think it's
    rather quirky and that it would be nice if a less quirky language had
    come about to fill C's niche (C++ is not less quirky than C).

---
+++ kernel/include/string.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <stddef.h>
+
+int memcmp(const void*, const void*, size_t);
+void* memcpy(void* restrict, const void* restrict, size_t);
+void* memmove(void*, const void*, size_t);
+void* memset(void*, int, size_t);
