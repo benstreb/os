b17cccfb9a9ac0fe14158366c678781711701e13
Benjamin Streb <Bstreb@gmail.com>
2014-12-27 22:42:56 -0800

    Added implementation of memcpy
    Still untested. It's interesting that memcpy returns dest. I think I
    agree with the stylistic suggestion that functions that mutate their
    arguments should not output anything (except an error code or something
    like that, perhaps). These functions are mandated by libgcc, so it's not
    under my control in this case.
    I also find it interesting, if unsurprising, that memcpy makes the
    caller enforce non-overlapping regions.

--- kernel/arch/i386/string.c
+++ kernel/arch/i386/string.c
@@ -15,3 +15,13 @@ int memcmp(const void *s1, const void *s2, size_t n)
     return 0;
 }
 
+void *memcpy(void *restrict dest, const void *restrict src, size_t n)
+{
+    char *src_blob = (char*) src;
+    char *dest_blob = (char*) dest;
+    for (size_t i = 0; i < n; i++)
+    {
+        dest_blob[i] = src_blob[i];
+    }
+    return dest;
+}
