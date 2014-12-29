bcd33955b3d60b1e8616487d75f0f9b7662a8f11
Benjamin Streb <Bstreb@gmail.com>
2014-12-28 23:03:19 -0800

    Added memmove
    As I stated before, I just changed the method name then made memcpy call
    memmove. GCC and clang should both be able to inline memmove into
    memcpy, but I'd like it to be more explicit that memcpy should really
    just be the same body of code with a slightly different interface.
    Something to look up on another day, I guess.

--- kernel/arch/i386/string.c
+++ kernel/arch/i386/string.c
@@ -17,6 +17,11 @@ int memcmp(const void *s1, const void *s2, size_t n)
 
 void *memcpy(void *restrict dest, const void *restrict src, size_t n)
 {
+    return memmove(dest, src, n);
+}
+
+void *memmove(void *dest, const void *src, size_t n)
+{
     char *src_blob = (char*) src;
     char *dest_blob = (char*) dest;
     if (src_blob > dest_blob)
